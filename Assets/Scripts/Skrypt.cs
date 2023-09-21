d770760de19d14bacc7f2a55ce4a6b045cba75ef
Filip Tobisz <czechune@gmail.com>
2023-09-21 13:26:53 +0200

    dodanie sceny
    dodanie assetów i spritów oraz colliderów i początek poruszania się postaci

---
+++ Assets/Scripts/Skrypt.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skrypt : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement);
+
+
+
+    }
+}
